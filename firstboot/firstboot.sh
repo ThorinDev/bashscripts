c2eafd711533889bfc9027822cc9a6725f413dd2
Chris <c.nethercott@icloud.com>
2016-11-21 18:00:08 +0000

    Create firstboot.sh

---
+++ firstboot/firstboot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+sudo apt-get update -y
+sudo apt-get dist-update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y
+curl -L --silent https://git.io/vXFbI | bash
+echo "FirstBoot was instaled"
