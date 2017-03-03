d28542601f5cdfcb6a4513f1505cacb121a83ab0
Chris <c.nethercott@icloud.com>
2017-03-03 17:31:41 +0000

    Update firstboot.sh

--- firstboot/firstboot.sh
+++ firstboot/firstboot.sh
@@ -3,5 +3,6 @@ sudo apt-get update -y
 sudo apt-get dist-update -y
 sudo apt-get upgrade -y
 sudo apt-get install -y
+sudo rm -rf /etc/motd
 curl -L --silent https://git.io/vXFbI | bash
 echo "FirstBoot was instaled"
